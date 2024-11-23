0d16c34ab72d3a766b33bbfdf096fc0d5587745d
Bhuvanesh2001 <bhuvaneshsai24@gmail.com>
2024-11-23 09:24:20 -0500

    changes in mobile verification

--- src/MobileVerification.js
+++ src/MobileVerification.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 export default function MobileVerification() {
-    return <div>MobileVerification</div>;
+    return (
+        <div className="h-screen bg-[linear-gradient(to_bottom,_#000000,_#00007b)] flex flex-col items-center justify-center">
+            <div className="h-[350px] w-[320px] bg-white rounded-[20px] opacity-40">
+                <div className="flex justify-center mt-[20px] text-xl">Sign Up</div>
+                Enter Mobile Number
+            </div>
+        </div>
+    );
 }
-    
